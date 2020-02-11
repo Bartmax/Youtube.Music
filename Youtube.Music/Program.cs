2fc4c08643e13bb8a714eea0d3a75ef1af96cc02
Bart Calixto <bart@dot.uy>
2020-02-10 21:42:28 -0300

    Add project files.

---
+++ Youtube.Music/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using WebWindows;
+
+namespace Youtube.Music
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var window = new WebWindow("Youtube Music");
+            window.NavigateToUrl("https://music.youtube.com");
+            window.WaitForExit();
+        }
+    }
+}
